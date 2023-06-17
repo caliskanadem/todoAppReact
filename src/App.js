1db8e10bd3c42d5b780d6e292138f6d0b8dbbc4e
caliskanadem <ad3m.calikan@gmail.com>
2023-06-17 20:01:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("All");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler(todos);
+    saveLocalTodos();
+  }, [todos, status]); //eslint-disable-line
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  //! save to local
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  // const getLocalTodos = () => {
+  //   if (localStorage.getItem("todos") === null) {
+  //     localStorage.setItem("todos", JSON.stringify([]));
+  //   } else {
+  //     setTodos(JSON.parse(localStorage.getItem("todos")));
+  //   }
+  // };
+
+  const getLocalTodos = () => {
+    const localTodos = localStorage.getItem("todos");
+    if (localTodos && JSON.parse(localTodos).length > 0) {
+      setTodos(JSON.parse(localTodos));
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>MY TODO LIST</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        filterHandler={filterHandler}
+        setStatus={setStatus}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
+  //   const inputTextHandler = () => {
+  //   };
+
+  const [alertWarning, setAlertWarning] = useState(false);
+  const [alertSuccess, setAlertSuccess] = useState(false);
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+
+    const isEmpty = (str) => !str.trim().length;
+    if (isEmpty(inputText)) {
+      setAlertWarning(true);
+      setTimeout(() => {
+        setAlertWarning(false);
+      }, 1500);
+    } else {
+      setAlertSuccess(true);
+      setTimeout(() => {
+        setAlertSuccess(false);
+      }, 1500);
+      setTodos([
+        ...todos,
+        { text: inputText, completed: false, id: Math.random() },
+      ]);
+    }
+
+    setInputText("");
+  };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+  // console.log(todos);
+  return (
+    <form onSubmit={submitTodoHandler}>
+      <div className="search">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="Add..."
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+        />
+        <button className="todo-button" type="submit">
+          <i className="fas fa-plus-circle"></i>
+        </button>
+      </div>
+
+      <div className="select">
+        <select name="todos" className="filter-todo" onChange={statusHandler}>
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+      <div className="alert-wrapper">
+        {alertWarning ? (
+          <div className="alert-warning">
+            <div>This field cannot be empty!</div>
+          </div>
+        ) : (
+          ""
+        )}
+        {alertSuccess ? (
+          <div className="alert-success">
+            <div>Success</div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default Form;
